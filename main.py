6ae7ae00304ee4180860dd5772c01696a54b16a3
ksrija <srija.karmakar@sudlife.in>
2022-08-22 12:20:38 +0530

    Fetch comments with id = id

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ app= FastAPI()
 @app.get("/")
 def index():
     return {"data":{"name":"John"}}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"commments":"Unpublished comments"}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    "Fetch blog with id = id"
+    return {"data":id}
+
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    "Fetch comments of id=id"
+
+    return {"comments":{"This is wonderful!","That is amazing."}}
+
+
